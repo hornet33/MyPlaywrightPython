7506a22b0034c0ddc890521a03ba6da34847f64f
rahulpradhan <rahul_pradhan@outlook.com>
2024-01-30 09:24:11 +0530

    Added custom markers and pytest.ini file

--- tests/test_contact_us.py
+++ tests/test_contact_us.py
@@ -15,6 +15,7 @@ def test_contact_us_run(playwright: Playwright) -> None:
     contact_us_page = ContactUs(page)
 
     contact_us_page.navigate()
+    page.wait_for_load_state(state="networkidle")  # This is like a wait statement in Playwright
     expect(contact_us_page.name_field).to_be_visible()
     contact_us_page.fill_form("Test", "Address", "abc@def.com", "1234567890",
                               "Test Sub", "This is a test message.")
@@ -29,7 +30,7 @@ def test_contact_us_run(playwright: Playwright) -> None:
 
 @pytest.mark.skip(reason="Skip demo only")  # This pytest annotation/marker is to skip this test during execution
 def test_skip_demo(playwright: Playwright) -> None:
-    print("This is just a pytest mark.skip demo function - doesn't do anything")
+    print("This will never be printed - just a pytest mark.skip demo function that doesn't do anything")
 
 
 @pytest.mark.xfail(reason="XFail demo only")  # This pytest marker is to mark tests which are expected to fail
@@ -41,6 +42,7 @@ def test_xfail_demo(playwright: Playwright) -> None:
     # Initializing the page object
     contact_us_page = ContactUs(page)
     contact_us_page.navigate()
+    page.wait_for_load_state(state="networkidle")  # This is like a wait statement in Playwright
     expect(contact_us_page.name_field).to_be_hidden()  # Expect() will fail
 
     context.close()
